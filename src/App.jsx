5aa8a8af3fdf4a357dc5fc64af4c7c58ff3520a0
Sanwicklith <kcvmd22@gmail.com>
2024-01-26 16:22:49 +0200

    exercise_1.6

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,26 @@
-import Content from "./Content";
-import Header from "./Header";
-import Total from "./Total";
+import { useState } from "react";
 
-function App() {
-  const course = "Half Stack application developemnt";
-  const part1 = "Fundamentals of React";
-  const exercise1 = 10;
-  const part2 = "Using props to pass data";
-  const exercise2 = 7;
-  const part3 = "State of a component";
-  const exercise3 = 14;
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   return (
-    <>
-      <Header course={course} />
-      <Content
-        part1={part1}
-        part2={part2}
-        part3={part3}
-        exercise1={exercise1}
-        exercise2={exercise2}
-        exercise3={exercise3}
-      />
-      <Total
-        exercise1={exercise1}
-        exercise2={exercise2}
-        exercise3={exercise3}
-      />
-    </>
+    <div>
+      <div className="btn-container">
+        <button onClick={() => setGood(good + 1)}>Good</button>
+        <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
+        <button onClick={() => setBad(bad + 1)}>Bad</button>
+      </div>
+      <h2>Statistics</h2>
+      <div>
+        <p>Good {good}</p>
+        <p>Neutral {neutral} </p>
+        <p>Bad {bad} </p>
+      </div>
+    </div>
   );
-}
+};
 
 export default App;
