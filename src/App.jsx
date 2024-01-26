e3e34df5afe58a430e8f9285f58bafc34a6ff04f
Sanwicklith <kcvmd22@gmail.com>
2024-01-26 16:49:01 +0200

    exercise 1.7

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,9 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-
+  const all = good + neutral + bad;
+  const avg = (good - bad) / all;
+  const positive = (good / all) * 100;
   return (
     <div>
       <div className="btn-container">
@@ -18,6 +20,9 @@ const App = () => {
         <p>Good {good}</p>
         <p>Neutral {neutral} </p>
         <p>Bad {bad} </p>
+        <p>All {all} </p>
+        <p>Average {avg} </p>
+        <p>Positive {positive}% </p>
       </div>
     </div>
   );
